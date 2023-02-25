40cd983665cd7a3702c930d2b13ada0866590cf8
Jartius <JStavey13@users.noreply.github.com>
2023-02-25 11:08:05 -0500

    finished up the User model

---
+++ main/models/User.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+
+// schema for the user model
+const userSchema = new Schema(
+  {
+    username: { 
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // a regex for email validation
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+
+    thoughts: [
+        //references the Thought model
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+
+    friends: [
+        // references the User model
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+},
+{
+    //allows virtuals to be included
+    toJSON: {
+        virtuals: true,
+      },
+      id: false,
+}
+);
+
+// virtual that returns the length of the friends field 
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  })
+
+  // exports the model
+  module.exports = User;
