1ef302c22cdbaf19201185950a54caef4bff0ca9
Jartius <JStavey13@users.noreply.github.com>
2023-02-28 22:09:22 -0500

    added thought routes and added to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,15 @@
+const Thought = require('../models/Thought')
 const { Reaction } = require('../models')
+
+const thoughtController = {
+    getThoughts(req, res) {
+        Thought.find()
+          .then((users) => res.json(users))
+          .catch((err) => res.status(500).json(err));
+      },
+}
+
+
+
+
+module.exports = thoughtController
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const {
+    getThoughts
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts)
+
+
+
+
+module.exports = router;
