4eeb34151cc156bfab6056ef9f44e9af9474208a
Jartius <JStavey13@users.noreply.github.com>
2023-03-02 09:36:05 -0500

    added video to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # social-network-api
 An api for a social network where users share their thoughts and interact with each other
+
+## usage video
+![This video is a walkthrough of the Social Network API program.](./assets/social%20network%20api%20vid.webm)
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,12 +5,21 @@ const {
     getSingleThought,
     deleteThought,
     updateThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thought-controller')
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+
+// /api/thoughts/:thoughtId/reaction
+router.route('/:thoughtId/reaction').post(addReaction);
+
+// /api/thoughts/:thoughtId/reaction/:reactionId
+router.route('/:thoughtId/reaction/:reactionId').delete(removeReaction);
 
 
 module.exports = router;
